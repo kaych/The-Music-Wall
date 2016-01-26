47d098240242607dd882c5de30769ffb3b23e7d5
Kay Cheng <puiki.cheng@gmail.com>
2016-01-26 01:01:04 +0000

    Create new migration for users

---
+++ db/migrate/20160126005453_add_users.rb
@@ -0,0 +1,13 @@
+class AddUsers < ActiveRecord::Migration
+  
+  def change
+
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.timestamps null: false
+    end
+
+  end
+
+end
