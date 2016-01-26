b7b2a13d2b3125f3fb1ef9eedf327ad078296149
Kay Cheng <puiki.cheng@gmail.com>
2016-01-26 22:44:18 +0000

    Edit associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
-  has_many :musics, through: :upvotes
+  has_many :musics#, through: :upvotes
   has_many :upvotes
+  has_many :reviews
 
   validates :username, presence: true
   validates :password, presence: true
