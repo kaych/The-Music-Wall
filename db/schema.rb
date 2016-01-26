71457b9193c46c02b9c23b085437fa911c9b3904
Kay Cheng <puiki.cheng@gmail.com>
2016-01-26 01:30:39 +0000

    Update schema with new user table and new user id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125191313) do
+ActiveRecord::Schema.define(version: 20160126011437) do
 
   create_table "musics", force: :cascade do |t|
     t.string   "song_title"
     t.string   "author"
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
