9939dbf718d2eb8e31b50e8bcb90ae50c3b84717
Kay Cheng <puiki.cheng@gmail.com>
2016-01-26 22:43:59 +0000

    Edit associations with upvotes and reviews

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,8 +1,9 @@
 class Music < ActiveRecord::Base
 
   belongs_to :user
-  has_many :user, through: :upvotes
+  # has_many :user, through: :upvotes
   has_many :upvotes
+  has_many :reviews
 
   validates :song_title, presence: true
   validates :author, presence: true
