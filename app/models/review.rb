2d5e772b5967a2e68b960d6829573d2fd88af402
Kay Cheng <puiki.cheng@gmail.com>
2016-01-26 23:03:36 +0000

    Update validation constraints

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :music
-  
+
+  validates :like, presence: true, format: { with: /(like|dislike)/ }
+  validates :comment, presence: true, length: { minimum: 4 }
+
 end 
