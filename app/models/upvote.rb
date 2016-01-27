68a15e899bad21cdd3fea23bc0468be1b69aa9f8
Kay Cheng <puiki.cheng@gmail.com>
2016-01-27 07:33:44 +0000

    Content improvements

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -3,6 +3,4 @@ class Upvote < ActiveRecord::Base
   belongs_to :user 
   belongs_to :music
 
-  # validates :user_id, uniqueness: true 
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :musics#, through: :upvotes
+  has_many :musics
   has_many :upvotes
   has_many :reviews
 
