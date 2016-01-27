abe568ff7286b615f0e733789e9b0b01e50d4f45
Kay Cheng <puiki.cheng@gmail.com>
2016-01-27 03:57:22 +0000

    Add ruby methods to be called in the erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :password, presence: true
 
+  def has_upvoted_song?(song)
+    self.upvotes.where(music_id: song.id).exists?
+  end
+
   def has_reviewed_song?(song)
     self.reviews.where(music_id: song.id).exists?
   end
